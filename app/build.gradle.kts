5a285c79d625de60021355ccf07bc5e5af83b415
santiago-peralta <speraltac@unal.edu.co>
2025-10-27 00:35:31 -0500

    MuckUp del menu / barra inferior

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.activity)
+    implementation(libs.cardview.v7)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
