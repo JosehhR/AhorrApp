6ea371a4a1967674ced7d813de572908ae8143aa
JosehhR <joramirezi@unal.edu.co>
2025-10-28 16:08:34 -0500

    Login y register integrados con realtimedatabase y auth de firebase

--- app/src/main/java/com/example/ahorrapp/LoginActivity.java
+++ app/src/main/java/com/example/ahorrapp/LoginActivity.java
@@ -1,16 +1,18 @@
 package com.example.ahorrapp;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.ahorrapp.databinding.LoginScreenBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginScreenBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,36 +20,52 @@ public class LoginActivity extends AppCompatActivity {
         binding = LoginScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnTabRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
+        // Inicializar Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.btnTabRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intent);
         });
 
-        binding.forgotPasswordText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
-                startActivity(intent);
-            }
+        binding.forgotPasswordText.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+            startActivity(intent);
         });
 
-        binding.loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = binding.emailInputLayout.getEditText().getText().toString();
-                String password = binding.passwordInputLayout.getEditText().getText().toString();
+        binding.loginButton.setOnClickListener(v -> loginUser());
+    }
 
-                String message = "Email: " + email + "\nContraseña: " + password;
+    private void loginUser() {
+        String email = binding.emailInputLayout.getEditText().getText().toString().trim();
+        String password = binding.passwordInputLayout.getEditText().getText().toString().trim();
 
-                new AlertDialog.Builder(LoginActivity.this)
-                    .setTitle("Datos de Inicio de Sesión")
-                    .setMessage(message)
-                    .setPositiveButton("Aceptar", null)
-                    .show();
-            }
-        });
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            showErrorDialog("El correo y la contraseña son obligatorios.");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Inicio de sesión exitoso, redirigir a PerfilActivity
+                        Intent intent = new Intent(LoginActivity.this, PerfilActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish(); // Cierra LoginActivity
+                    } else {
+                        // Si el inicio de sesión falla, mostrar un mensaje de error.
+                        String errorMessage = task.getException() != null ? task.getException().getMessage() : "La autenticación falló.";
+                        showErrorDialog("Error de Inicio de Sesión: " + errorMessage);
+                    }
+                });
+    }
+
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(LoginActivity.this)
+            .setTitle("Error")
+            .setMessage(message)
+            .setPositiveButton("Aceptar", null)
+            .show();
     }
 }
--- app/src/main/java/com/example/ahorrapp/PerfilActivity.java
+++ app/src/main/java/com/example/ahorrapp/PerfilActivity.java
@@ -1,24 +1,99 @@
 package com.example.ahorrapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ahorrapp.databinding.ActivityPerfilBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    private ActivityPerfilBinding binding;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_perfil);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityPerfilBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
+            loadUserProfile();
+        } else {
+            redirectToLogin();
+        }
+
+        binding.CerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                redirectToLogin();
+            }
         });
+    }
+
+    private void loadUserProfile() {
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String email = dataSnapshot.child("email").getValue(String.class);
+                    Double income = dataSnapshot.child("income").getValue(Double.class);
+                    String name = dataSnapshot.child("name").getValue(String.class);
+                    String frequency = dataSnapshot.child("paymentFrequency").getValue(String.class);
+
+
+
+                    binding.Email.setText(email);
+
+                    name = "@" + name;
+                    binding.Name.setText(name);
+
+                    binding.Frequency.setText(frequency);
+
+
+
+                    if (income != null) {
+                        // Formatear el número como moneda
+                        NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("es", "CO"));
+                        format.setMaximumFractionDigits(0);
+                        binding.Income.setText(format.format(income));
+                    } else {
+                        binding.Income.setText("$ 0");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(PerfilActivity.this, "Error al cargar los datos del perfil.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void redirectToLogin() {
+        Intent intent = new Intent(PerfilActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
